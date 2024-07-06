e5d95c22824e84324c58070ebe18e6db09f6d81d
nomanmujahid1144 <nomanmujahid.cs@gmail.com>
2024-07-06 20:27:33 +0500

    add day-10 complete the data-tool/ai-tool/folder-animation

--- src/app/page.js
+++ src/app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
         <AITool />
         <FoldingCardXScroll />
       </section>
-      <OurPartner />
+      {/* <OurPartner /> */}
       {/* <FrequentlyAsked /> */}
     </>
   );
