7ba4463183e4ab871c9e575a46e83eec6c3989b1
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-07-17 12:08:40 -0700

    add: text-animation-complete / some other things

--- src/app/components/navbar/NavLink.js
+++ src/app/components/navbar/NavLink.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
-export const NavLink = ({ navLinkName, navLink, extras, icon, iconImage, index, font }) => {
+export const NavLink = ({ navLinkName, navLink, extras, icon, iconImage, key, font }) => {
     const [isToggled, setIsToggled] = useState(false);
 
     const handleClick = () => {
@@ -14,21 +14,20 @@ export const NavLink = ({ navLinkName, navLink, extras, icon, iconImage, index, 
     return (
         <Link
             href={navLink} 
-            key={index} 
+            key={key} 
             onClick={handleClick} 
             className={`${extras} ${isToggled ? '!bg-black text-green ring-1 ring-green' : ''} h-inherit flex items-center justify-center transition-animation hover:shadow-inner`}
         >
             {icon ? (
                 <Image
                     src={iconImage}
-                    key={index + 1}
                     alt="icons"
                     width={18}
                     height={15}
                     className={`fill-current w-6 h-6 md:w-4 md:h-4 ${isToggled ? 'text-green' : ''}`}
                 />
             ) : (
-                <span key={index + 2} className={`${font} text-black uppercase lg:text-sm md:text-xs font-medium md:leading-66 leading-none tracking-space60 inner-text`}>
+                <span className={`${font} text-black uppercase lg:text-sm md:text-xs font-medium md:leading-66 leading-none tracking-space60 inner-text`}>
                     {navLinkName}
                 </span>
             )}
