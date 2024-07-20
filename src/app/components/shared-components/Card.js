a6a53ffb464b7022a904e965bbba1fbeb4bf7470
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-07-20 09:10:32 -0700

    add: set animation issues or some text issues on mobile version

--- src/app/components/shared-components/Card.js
+++ src/app/components/shared-components/Card.js
@@ -1,6 +1,6 @@
 export const Card = ({children, extras, key}) => {
     return (
-        <div key={key} className={`bg-yellowLight w-full flex justify-center items-center ${extras}`}>
+        <div key={key} className={`bg-yellowLight fade-in-out w-full flex justify-center items-center ${extras}`}>
             {children}
         </div>
     )
