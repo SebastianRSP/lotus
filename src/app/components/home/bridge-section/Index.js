75eaad02476e95c588ddd245c27d2ed6e8fbdfff
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-08-31 02:09:38 +0500

    add: make some changes

--- src/app/components/home/bridge-section/Index.js
+++ src/app/components/home/bridge-section/Index.js
@@ -1,5 +1,11 @@
 export const BridgeSection = () => {
     return (
-        <div>Noman</div>
+        <div className="bg-black h-screen forced-full-width">
+            <div className="flex justify-center w-full">
+                <h4 className="2xl:text-6xl text-5xl 2xl:leading-66 leading-56 text-white font-extralight text-center">
+                    We’re building <br /> the world’s largest<br /> content delivery network
+                </h4>
+            </div>
+        </div>
     )
 }
