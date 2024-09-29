b4aa6f73d5e4c7963e4bee15755e9a1e88700cc7
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-09-29 15:17:50 +0500

    add: setting tweeks

--- src/app/components/home/Index.js
+++ src/app/components/home/Index.js
@@ -41,24 +41,25 @@ export const IndexHome = () => {
                     </div>
                     <div className=" grid grid-cols-12 h-available justify-end items-end">
                         {/* 2xl:gap-4.5 xl:gap-0 md:gap-12 gap-0 */}
-                        <div className="lg:col-span-7 col-span-11 2xl:pl-100 lg:pl-14 md:pl-10 pl-5 grid 2xl:grid-rows-2 lg:grid-rows-3 grid-rows-5 items-end h-available">
-                            <div className="flex flex-col gap-6 md:self-center self-end lg:row-span-2 row-span-3">
-                                <h2 id="home-hero-heading" className="font-extralight 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl text-3xl 2xl:leading-84 xl:leading-66 lg:leading-54 md:leading-42 leading-9">
+                        <div className="lg:col-span-7 col-span-11 2xl:pl-100 lg:pl-14 md:pl-10 pl-5 grid 2xl:grid-rows-2 xl:grid-rows-6 lg:grid-rows-5 md:grid-rows-2 grid-rows-5 items-end h-available">
+                            <div className="flex flex-col gap-6 md:self-center self-end xl:row-span-4 lg:row-span-4 md:row-span-4 row-span-3">
+                                {/* xl:text-6xl xl:leading-66  */}
+                                <h2 id="home-hero-heading" className="font-extralight 2xl:text-7xl xl:text-3.5r lg:text-5xl md:text-4xl text-3xl 2xl:leading-84 xl:leading-62 lg:leading-54 md:leading-42 leading-9">
                                     Decentralized<br /> data infrastructure<br /> for{" "}
                                     {/* <TextSwapper /> */}
                                     <TextTyping />
                                 </h2>
-                                <p id="home-hero-paragraph" className="2xl:text-2xl xl:text-xl md:text-xl sm:text-lg text-sm">Data mobility. Without limits.</p>
-                                <div id="home-hero-button" className="md:pt-6 pt-3">
+                                <p id="home-hero-paragraph" className="2xl:text-2xl lg:text-lg sm:text-base text-sm">Data mobility. Without limits.</p>
+                                <div id="home-hero-button" className="2xl:pt-6 pt-3">
                                     <DefaultBtn
                                         btnText={'Get Started'}
                                     />
                                 </div>
                             </div>
-                            <div id="home-hero-bullets" className="flex flex-col lg:row-span-1 row-span-2 2xl:pb-10 pb-5 w-fit">
+                            <div id="home-hero-bullets" className="flex flex-col xl:row-span-2 lg:row-span-1 md:row-span-2 row-span-2 2xl:pb-10 pb-5 w-fit">
                                 <div className="border-t-[0.2px] border-t-black border-opacity-20">
                                     {bullets.map((bullet, index) => (
-                                        <div key={index} className="flex items-center gap-3 border-b-[0.2px] border-b-black border-opacity-20 xl:py-5 py-3">
+                                        <div key={index} className="flex items-center gap-3 border-b-[0.2px] border-b-black border-opacity-20 2xl:py-5 py-2">
                                             <Image
                                                 src={tickIcon}
                                                 className="2xl:w-4 -mb-2"
@@ -66,7 +67,7 @@ export const IndexHome = () => {
                                                 width={10}
                                                 height={10}
                                             />
-                                            <p className="2xl:text-2xl xl:text-xl md:text-xl sm:text-lg text-sm font-normal xl:leading-26 md:leading-22 leading-5">{bullet.buttet}</p>
+                                            <p className="2xl:text-2xl lg:text-lg sm:text-base text-sm font-normal xl:leading-26 md:leading-22 leading-5">{bullet.buttet}</p>
                                         </div>
                                     ))}
                                 </div>
--- src/app/components/home/text-animation/TextTyping.js
+++ src/app/components/home/text-animation/TextTyping.js
@@ -6,9 +6,9 @@ export const TextTyping = () => {
                 <div className="inline-block bg-green w-auto px-px">
                     <span
                         id='text-typing-animation'
-                        className="font-bold 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl text-3xl 2xl:leading-84 xl:leading-66 lg:leading-54 md:leading-42 leading-9 cursor-pointer inline-flex w-max"
+                        className="font-bold 2xl:text-7xl lg:text-5xl md:text-4xl text-3xl 2xl:leading-84 lg:leading-54 md:leading-42 leading-9 cursor-pointer inline-flex w-max"
                     ></span>
-                    <span className="font-300 relative -top-1 animate-blink">
+                    <span className="font-thin relative -top-1 animate-blink">
                         |
                     </span>
                 </div>
