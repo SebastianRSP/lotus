446218e4309ef8a6c33ee210ce7d9efb34bfeefe
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-10-05 18:59:19 +0500

    add: complete the about us page

--- src/app/components/white-strap-navbar/WhiteStrap.js
+++ src/app/components/white-strap-navbar/WhiteStrap.js
@@ -13,7 +13,7 @@ export const WhiteStrap = () => {
                 <div className={`${isDark ? 'bg-transparent rounded-tr-lg' : 'bg-gray-light rounded-tr-lg rounded-tl-lg'}  h-full w-full`}></div>
             </div>
             <div className={`${isDark ? 'bg-transparent' : 'bg-gray-light'} order-1 md:order-2 col-span-4 md:col-span-full 2xl:h-18 lg:h-14 h-16 z-10 lg:w-28.5p md:w-1/5 w-full relative`}>
-                <div className={` bg-white h-full w-full md:rounded-bl-[43px] rounded-br-[43px]`}></div>
+                <div className={` bg-white h-full w-full md:rounded-bl-[32px] rounded-br-[32px]`}></div>
             </div>
             <div className={`${isDark ? 'bg-transparent rounded-tl-lg' : 'bg-white'}  col-span-5 order-3 md:order-3 md:col-span-full 2xl:h-18 lg:h-14 h-16 z-10 lg:w-35.75p md:w-40p relative`}>
                 <div className={`${isDark ? 'bg-transparent rounded-tl-lg' : 'bg-gray-light rounded-tl-lg md:rounded-tr-lg'}  h-full w-full`}></div>
