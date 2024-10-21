fe305aee21bb1782439f78957623d6cbcdc3da5b
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-10-21 19:15:26 +0500

    add: change bridge animation

--- src/app/components/home/bridge/Index.js
+++ src/app/components/home/bridge/Index.js
@@ -34,7 +34,7 @@ export const Bridge = () => {
             {/* 2xl:px-40 xl:px-20 2xl:py-40 xl:py-20 */}
             <div className="bridge-overlay bg-none">
                 <div className="bridge-wrapper-1 md:overflow-hidden bg-black bridge-gridroom">
-                    <div className="relative opacity-50 bridge-window bridge-window-full">
+                    <div className="relative top-0 lg:top-auto opacity-50 bridge-window bridge-window-full">
                         <GridGreen />
                         {/* <div className="absolute inset-0 z-20 bg-black backdrop-blur-1 opacity-70 bridge-bg-blur" /> */}
                         <div className="absolute overflow-hidden inset-0 w-full h-full flex justify-center items-center z-10 ">
@@ -82,66 +82,6 @@ export const Bridge = () => {
                 <div className="cover-object conver-object-vertical bg-gray-light"></div>
                 <div className="cover-object conver-object-vertical bg-gray-light"></div>
             </div>
-            
-            {/* <div className="bridge-initial">
-                <div className="bg-black relative">
-                    <Gridlines />
-                    <div className="absolute inset-0 z-20 bg-black backdrop-blur-1 rounded-lg opacity-70 bridge-bg-blur" />
-                    <div className="absolute inset-0 z-20 2xl:py-56 xl:py-40 py-16">
-                        <div className="flex flex-col justify-start w-full items-left 2xl:px-40 xl:px-28 lg:px-10 px-5">
-                            <div className="flex flex-col">
-                                <h4 className="bridge-hero-text opacity-0 2xl:text-8xl xl:text-7xl lg:text-5xl md:text-4xl text-28 2xl:leading-102 xl:leading-78 lg:leading-54 md:leading-42 leading-9 font-extralight text-[#D0D0D0]">
-                                    We’re building <br /> the world’s largest<br /> content delivery network
-                                </h4>
-                                <div className='text-green 2xl:pt-56 xl:pt-28 relative'>
-                                    <div className='absolute w-12/12 text-left flex justify-start gap-3'>
-                                        <h4 className="2xl:text-9xl xl:text-8xl lg:text-5xl md:text-4xl text-28 2xl:leading-132 xl:leading-102 lg:leading-54 md:leading-42 leading-9 uppercase font-bold">
-                                            The Bridge
-                                        </h4>
-                                        <div className='border border-green rounded-full self-baseline'>
-                                            <span className='font-bold text-3xl leading-60 px-2'>
-                                                TM
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="2xl:pt-45r xl:pt-25r py-0">
-                                <h5 className="2xl:text-108 xl:text-6xl lg:text-5xl md:text-4xl text-28 2xl:leading-124 xl:leading-66 lg:leading-54 md:leading-42 leading-9 text-gray-light font-extralight ">
-                                    A single pane of glass <br /> connecting <br /> <span className="font-normal">web2</span> and <span className="font-normal">web3</span>
-                                </h5>
-                            </div>
-                            <div className="2xl:py-72 pt-28 text-center">
-                                <div className="flex flex-col gap-6">
-                                    {cardDetails.map((cardDetail, index) => (
-                                        <BridgeCard>
-                                            <div key={index} className="grid md:grid-cols-2 grid-cols-1 justify-center items-center 2xl:py-24 py-12 md:px-0 px-5">
-                                                <div className="md:my-0 my-20">
-                                                    <Image
-                                                        src={cardDetail.defaultImage}
-                                                        alt="default Image"
-                                                        width={40}
-                                                        height={40}
-                                                    />
-                                                </div>
-                                                <div className="flex flex-col justify-between gap-3 text-left lg:w-4/6 md:w-11/12 w-full">
-                                                    <span className="2xl:text-lg text-base font-normal leading-42 text-green">{cardDetail.category}</span>
-                                                    <h4 className="2xl:text-4xl lg:text-3xl text-2xl font-300 2xl:leading-42 lg:leading-9 leading-30 text-green 2xl:w-3/4 lg:w-4/5 w-5/6">
-                                                        {cardDetail.heading}
-                                                    </h4>
-                                                    <p className="2xl:text-2xl lg:text-xl text-base">
-                                                        {cardDetail.description}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </BridgeCard>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </section>
     )
 }
