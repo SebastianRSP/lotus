e0fe548f9036759820c6fc6aab145078abe14d32
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-11-08 22:58:42 +0500

    add:working on investor page animation

--- src/app/components/navbar/home/HomeNavLink.js
+++ src/app/components/navbar/home/HomeNavLink.js
@@ -34,7 +34,7 @@ export const HomeNavLink = ({ navLinkName, navLink, extras, icon, iconImage, id,
     
             setTimeout(() => {
                 router.push(navLink); // Navigate to the route after delay
-            }, 500);
+            }, 350);
         }
     }, [navLink, router, currentPath]);
 
