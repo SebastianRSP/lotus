a6ce861350d163512bdee173291384e46a053b52
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-11-11 15:57:06 +0500

    add: lottie animation changes

--- src/app/components/home/feature-section/Index.js
+++ src/app/components/home/feature-section/Index.js
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 // import rails from '../../../../../public/animations/rails.lottie';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-import snip from '../../../../../public/new-home-assets/snip.png';
+// import snip from '../../../../../public/new-home-assets/snip.png';
 import Lottie from 'lottie-react';
 
 export const FeatureSection = () => {
@@ -15,18 +15,18 @@ export const FeatureSection = () => {
                         <h4 className='lg:hidden block absolute top-0 2xl:-ml-96 lg:-ml-64 ml-0 bg-white z-30 2xl:pl-20 lg:pl-10 pl-0 2xl:pb-20 lg:pb-10 pb-6 font-extralight 2xl:text-6xl xl:text-5xl lg:text-2.5 md:text-4xl text-3xl 2xl:leading-60 xl:leading-54 lg:leading-46 md:leading-42 leading-9'>
                             Lotus powers the future of data storage by building superior rails for migration.
                         </h4>
-                        <Image 
+                        {/* <Image 
                             src={snip}
                             className='absolute w-fit h-available'
                             width={100}
                             height={100}
-                        />
-                        {/* <DotLottieReact
+                        /> */}
+                        <DotLottieReact
                             src='/animations/rails.lottie'
                             loop
                             autoplay
                             className="absolute w-fit h-available"
-                        /> */}
+                        />
                     </div>
                 </div>
                 <div className="lg:col-span-5 col-span-12 bg-white lg:pt-0 pt-8">
