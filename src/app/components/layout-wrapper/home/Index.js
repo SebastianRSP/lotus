7bbaf4c5aaefa3d35db16bef0e6c96f79a58646a
Noman Mujahid <nomanmujahid.cs@gmail.com>
2024-12-16 23:49:31 +0500

    add: make latest build

--- src/app/components/layout-wrapper/home/Index.js
+++ src/app/components/layout-wrapper/home/Index.js
@@ -5,31 +5,31 @@ import { useEffect, useRef } from "react";
 import { HomeNavbar } from "../../navbar/home/HomeNavbar";
 import { NewHomeLoading } from "../../loading/home/Index";
 
-// import { ScrollTrigger } from "gsap/all";
-// import { desktopNavbarAnimation } from "../../gsap-animations/DesktopNavbarAnimation";
-// import { newHomePageAnimation } from "../../gsap-animations/home/Index2";
-// import { smoothScroolling } from "../../gsap-animations/smoothScroll";
+import { ScrollTrigger } from "gsap/all";
+import { desktopNavbarAnimation } from "../../gsap-animations/DesktopNavbarAnimation";
+import { newHomePageAnimation } from "../../gsap-animations/home/Index2";
+import { smoothScroolling } from "../../gsap-animations/smoothScroll";
 
 export const NewHomePage = ({ children }) => {
 
     const pathname = usePathname();
     const containerRef = useRef();
 
-    // useEffect(() => {
-    //     if (typeof window !== 'undefined') {
-    //         try {
-    //             smoothScroolling();
-    //             newHomePageAnimation();
-    //             desktopNavbarAnimation();
-    //         } catch (error) {
-    //             console.error("Animation setup failed:", error);
-    //         }
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            try {
+                smoothScroolling();
+                newHomePageAnimation();
+                desktopNavbarAnimation();
+            } catch (error) {
+                console.error("Animation setup failed:", error);
+            }
     
-    //         return () => {
-    //             ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    //         };
-    //     }
-    // }, [pathname]);
+            return () => {
+                ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+            };
+        }
+    }, [pathname]);
     
 
     return (
